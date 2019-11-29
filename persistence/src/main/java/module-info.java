7cda2382376f1ec94c027ee295a4b31609c22ff8
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-11-29 16:47:04 +0100

    MySql db configured, initializing db with CommandLineRunner to populate db with two users. RestController /users configured to return all Users

--- persistence/src/main/java/module-info.java
+++ persistence/src/main/java/module-info.java
@@ -5,6 +5,10 @@ module persistence {
     requires spring.data.jpa;
     requires spring.context;
     requires java.persistence;
+    requires spring.security.core;
+    requires spring.boot;
 
     opens dev.ned.repositories to spring.core;
+
+    exports dev.ned.repositories.seeder to spring.beans;
 }
--- rest-api/src/main/java/dev/ned/rest/TestController.java
+++ rest-api/src/main/java/dev/ned/rest/TestController.java
@@ -1,13 +1,11 @@
 package dev.ned.rest;
 
-import dev.ned.model.Role;
 import dev.ned.model.User;
 import dev.ned.repositories.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,51 +18,8 @@ public class TestController {
         this.userRepository = userRepository;
     }
 
-    //    private UserQueryRepository userQueryRepository;
-//
-//    public TestController(UserQueryRepository userQueryRepository) {
-//        this.userQueryRepository = userQueryRepository;
-//    }
-
-//    @GetMapping
-//    public List<User> getAllUsers() {
-//        return this.userQueryRepository.getAllUsers();
-//    }
-//
-//    @GetMapping("/{id}")
-//    public Optional<User> getBoardGameById(@PathVariable Long id) {
-//        return this.userQueryRepository.getUserById(id);
-////                .orElseThrow(ResourceNotFoundException::new);
-//    }
-
-    @GetMapping("/")
-    public String sayHello() {
-        return "Hello!";
-    }
-
-    @GetMapping(value = "/q")
-    public List<String> sayHsello() {
-        List<String> l = new ArrayList<>();
-        l.add("zx");
-        l.add("as");
-        return l;
-    }
-
-    @GetMapping("/u")
-    public User user() {
-        User user = new User();
-        user.setId(1L);
-        user.setFirstName("Nedim");
-        user.setLastName("Kulovac");
-        user.setActive(true);
-        user.setEmail("email");
-        user.setPassword("pass");
-        user.setPermissions("perm");
-        List<Role> roles = new ArrayList<>();
-
-        Role role = new Role("ADMIN", user);
-        roles.add(role);
-        user.setRoles(roles);
-        return user;
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
     }
 }

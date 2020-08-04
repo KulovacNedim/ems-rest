0bb1d55f3585b86c9b36d599de20a2a3111af3c4
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-08-04 12:46:13 +0200

    module and maven dependencies cleaned

--- domain/src/main/java/module-info.java
+++ domain/src/main/java/module-info.java
@@ -3,7 +3,6 @@ module domain {
 
     requires java.persistence;
     requires com.fasterxml.jackson.annotation;
-    // requires jackson.annotations;
 
     opens dev.ned.model to org.hibernate.orm.core, spring.core, jackson.databind;
 }
--- persistence/src/main/java/module-info.java
+++ persistence/src/main/java/module-info.java
@@ -5,7 +5,6 @@ module persistence {
     requires spring.data.jpa;
     requires spring.context;
     requires java.persistence;
-//    requires spring.security.core;
     requires spring.boot;
     requires spring.security.core;
 

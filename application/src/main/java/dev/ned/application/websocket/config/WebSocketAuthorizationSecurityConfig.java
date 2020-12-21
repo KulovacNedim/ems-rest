3ff243f67e609590ef86ac798ca9813034b1b8c2
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-12-21 22:18:53 +0100

    WebSocket configured and secured

---
+++ application/src/main/java/dev/ned/application/websocket/config/WebSocketAuthorizationSecurityConfig.java
@@ -0,0 +1,20 @@
+package dev.ned.application.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketAuthorizationSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+    @Override
+    protected void configureInbound(final MessageSecurityMetadataSourceRegistry messages) {
+        // You can customize your authorization mapping here.
+        messages.simpDestMatchers("/secured/**").authenticated()
+                .anyMessage().authenticated();
+    }
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+}
